b2b0ef9807b906b6585131bf206dd88042b48cfa
Honlan <493722771@qq.com>
2014-09-09 23:38:27 +0800

    second page,figures to be PSed

--- xichao/js/xichao.js
+++ xichao/js/xichao.js
@@ -1,5 +1,30 @@
-var page1_touch = false;
+var page1_touch = false,
+page2_touch = false,
+$main = $( '#pt-main' ),
+$pages = $main.children( 'div.pt-page' ),
+pagesCount = $pages.length,
+current = 0,
+isAnimating = false,
+endCurrPage = false,
+endNextPage = false,
+animEndEventNames = {
+	'WebkitAnimation' : 'webkitAnimationEnd',
+	'OAnimation' : 'oAnimationEnd',
+	'msAnimation' : 'MSAnimationEnd',
+	'animation' : 'animationend'
+},
+// animation end event name
+animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ],
+// support css animations
+support = Modernizr.cssanimations;
+
 $(document).ready(function(){
+	$pages.each( function() {
+		var $page = $( this );
+		$page.data( 'originalClassList', $page.attr( 'class' ) );
+	} );
+	$pages.eq( current ).addClass( 'pt-page-current' );
+
 	$("#logo_blue").mouseenter(function(){
 		if(!page1_touch){
 			page1_touch = true;
@@ -21,27 +46,29 @@ $(document).ready(function(){
 					});
 				},300);
 			});
-			$("#logo_blue").fadeOut(900,function(){
+			$("#logo_blue").fadeTo(900,0.3,function(){
 				setTimeout(function(){
-					$("#logo_blue").fadeIn(400,function(){
+					$("#logo_blue").fadeTo(400,0.7,function(){
 						setTimeout(function(){
-							$("#logo_blue").fadeOut(900);
+							$("#logo_blue").fadeTo(900,0);
 						},200);
 					});
 				},300);
 			});
-			$("#logo_tran").fadeIn(900,function(){
+			$("#logo_tran").fadeTo(900,0.7,function(){
 				setTimeout(function(){
-					$("#logo_tran").fadeOut(400,function(){
+					$("#logo_tran").fadeTo(400,0.3,function(){
 						setTimeout(function(){
-							$("#logo_tran").fadeIn(900);
+							$("#logo_tran").fadeTo(900,1);
 						},200);
 					});
 				},300);
 			});
 			setTimeout(function(){
-				$(".previous").animate({opacity:"0.6",top:"-=2%"},1000);
-				$(".next").animate({opacity:"0.6",top:"-=2%"},1000);
+				$("#previous1").show();
+				$("#next1").show();
+				$("#previous1").animate({opacity:"0.6",top:"-=2%"},1000);
+				$("#next1").animate({opacity:"0.6",top:"-=2%"},1000);
 			},2500);
 		}
 	});
@@ -52,4 +79,113 @@ $(document).ready(function(){
 	$(".pt-touch-button").mouseleave(function(){
 		$(this).css("opacity","0.6");
 	});
+	$(".next").click(function() {
+		if( isAnimating ) {
+			return false;
+		}
+		nextPage(true);
+	});
+	$(".previous").click(function() {
+		if( isAnimating ) {
+			return false;
+		}
+		nextPage(false);
+	});
 });
+function nextPage( direction ) {
+
+	if( isAnimating ) {
+		return false;
+	}
+
+	isAnimating = true;
+
+	var $currPage = $pages.eq( current );
+
+	if(direction){
+		if( current < pagesCount - 1 ) {
+			++current;
+		}
+		else {
+			current = 0;
+		}
+
+		var $nextPage = $pages.eq( current ).addClass( 'pt-page-current' ),
+		outClass = 'pt-page-moveToLeftFade', inClass = 'pt-page-moveFromRightFade';
+
+		$currPage.addClass( outClass ).on( animEndEventName, function() {
+			$currPage.off( animEndEventName );
+			endCurrPage = true;
+			if( endNextPage ) {
+				onEndAnimation( $currPage, $nextPage );
+			}
+		} );
+
+		$nextPage.addClass( inClass ).on( animEndEventName, function() {
+			$nextPage.off( animEndEventName );
+			endNextPage = true;
+			if( endCurrPage ) {
+				onEndAnimation( $currPage, $nextPage );
+			}
+		} );
+
+		if( !support ) {
+			onEndAnimation( $currPage, $nextPage );
+		}
+	}
+	else{
+		if( current > 0 ) {
+			--current;
+		}
+		else {
+			current = pagesCount - 1;
+		}
+
+		var $nextPage = $pages.eq( current ).addClass( 'pt-page-current' ),
+		outClass = 'pt-page-moveToRightFade', inClass = 'pt-page-moveFromLeftFade';
+
+		$currPage.addClass( outClass ).on( animEndEventName, function() {
+			$currPage.off( animEndEventName );
+			endCurrPage = true;
+			if( endNextPage ) {
+				onEndAnimation( $currPage, $nextPage );
+			}
+		} );
+
+		$nextPage.addClass( inClass ).on( animEndEventName, function() {
+			$nextPage.off( animEndEventName );
+			endNextPage = true;
+			if( endCurrPage ) {
+				onEndAnimation( $currPage, $nextPage );
+			}
+		} );
+
+		if( !support ) {
+			onEndAnimation( $currPage, $nextPage );
+		}
+	}
+
+	if(current == 1&&!page2_touch){
+		page2_touch = true;
+		setTimeout(function(){
+			$("#logo_v").fadeIn(2000,function(){
+				$("#previous2").show();
+				$("#next2").show();
+				$("#previous2").animate({opacity:"0.6",top:"-=2%"},1000);
+				$("#next2").animate({opacity:"0.6",top:"-=2%"},1000);
+			});
+		},2000);
+	}
+}
+
+function onEndAnimation( $outpage, $inpage ) {
+	endCurrPage = false;
+	endNextPage = false;
+	resetPage( $outpage, $inpage );
+	isAnimating = false;
+}
+
+function resetPage( $outpage, $inpage ) {
+	$outpage.attr( 'class', $outpage.data( 'originalClassList' ) );
+	$inpage.attr( 'class', $inpage.data( 'originalClassList' ) + ' pt-page-current' );
+}
